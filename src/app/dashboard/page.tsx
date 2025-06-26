b081a486f57a4b0e1fb14ec6b51deb4eaa0de8d2
Neeraj Kumar Sharma <neerajkumarsharma@Neerajs-MacBook-Pro.local>
2025-06-26 17:05:41 +0530

    updated metadata

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,30 @@ import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import TodoClient from "./_components/TodoClient";
 
+export const metadata = {
+  title: "Todo App – Simple & Secure",
+  description: "A minimal todo app with Google/GitHub sign-in, powered by Next.js, Prisma & PostgreSQL.",
+  keywords: ["todo", "nextjs", "prisma", "postgresql", "nextauth", "task manager"],
+  authors: [{ name: "NeerajNero", url: "https://github.com/NeerajNero/TodoAppWithAuth" }],
+  creator: "NeerajNero",
+  openGraph: {
+    title: "Todo App – Next.js + Prisma",
+    description: "A simple yet powerful todo app with secure login and efficient task management.",
+    url: "https://todo-app-with-auth-phi.vercel.app/",
+    siteName: "Todo App",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Todo App – Next.js + Prisma",
+    description: "Secure login, fast performance. Your todo manager, redefined.",
+    images: ["https://your-vercel-url.vercel.app/og-image.png"],
+    creator: "@yourhandle",
+  },
+};
+
+
 export default async function DashboardPage() {
   const session = await auth();
   if (!session?.user?.id) redirect("/unauthorized");
